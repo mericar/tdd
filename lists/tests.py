1f26463f39cb028c17c461c3a2427abcb75bfe0d
mirkmasterC <mirkocarich@gmail.com>
2016-04-17 19:16:33 -0700

    created tests for tables

--- lists/tests.py
+++ lists/tests.py
@@ -16,8 +16,16 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
-		
 
+	def test_homepage_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new item in the list'
+
+		response = home_page(request)
+
+		self.assertIn('A new item in the list', response.content.decode())
+		expected_html =render_to_string('home.html', {'new_item_text':'A new item in the list'})
 
 
 
