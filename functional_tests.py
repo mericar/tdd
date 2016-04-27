5dfcf6d0d6c927ca02a90285fb3da6c4df657233
mirkmasterC <mirkocarich@gmail.com>
2016-04-26 22:02:09 -0700

    updated functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(5)
+		self.browser.implicitly_wait(10)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -30,12 +30,11 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
-
+		inputbox.send_keys('the first')
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('1: the first')
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very
 		# methodical)
@@ -43,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('1: the first')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
 		# Edith wonders whether the site will remember her list. Then she sees
