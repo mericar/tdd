859bb2b7e89dfaa017dcfb799881cd6c038fe4da
mirkmasterC <mirkocarich@gmail.com>
2016-05-07 16:58:35 -0700

    added separate lists, modified Django url hierarchy

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name="view_list"),
+	url(r'^$', list_views.home_page, name='home'),
+	url(r'^lists/', include(list_urls)),
+	# url(r'^admin/', include(admin.site.urls)),
 ]
